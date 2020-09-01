9b5cd7816bbe9ad8726bb3ab2e9fe568bee0c156
YaaqovShkifati <70407221+YaaqovShkifati@users.noreply.github.com>
2020-09-01 11:04:24 -0400

    Create Lab1.java

---
+++ Lab1.java
@@ -0,0 +1,19 @@
+/**
+The Lab1 class simply displays hello world.
+*/
+public class Lab1(){
+
+public Lab1(){
+System.out.println("Hello world");
+}
+
+/** 
+The main method.
+*/
+public static void main(String[] args){
+
+// insantiating an object of the lab1 class.
+Lab1 test = new Lab1();  
+ 
+    }
+}
